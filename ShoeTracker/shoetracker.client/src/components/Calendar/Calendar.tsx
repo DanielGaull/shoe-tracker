0f3f396de8f4e90164039a4d57fe81b077dd3b3e
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-10-09 13:08:57 -0400

    Weekly summary

--- ShoeTracker/shoetracker.client/src/components/Calendar/Calendar.tsx
+++ ShoeTracker/shoetracker.client/src/components/Calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { convertJsWeekdayToIndex, getMonday, weekdays } from '../../util/util';
-import { Activity } from '../../types/activity';
+import { addTimes, convertJsWeekdayToIndex, distanceAsMiles, getMonday, roundTo, timeToString, weekdays } from '../../util/util';
+import { Activity, Time } from '../../types/activity';
 import ActivityCard from '../../pages/ActivityList/ActivityCard';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
@@ -11,15 +11,19 @@ const getDaysInMonth = (month: number, year: number): number => {
     return new Date(year, month, 0).getDate();
 };
 
-type Day = { 
+interface Day { 
     date: number,
     isToday: boolean,
-    exists: true,
     month: number,
     year: number,
-} | { 
-    exists: false,
-};
+}
+
+interface WeeklySummary {
+    activities: number;
+    subActivities: number;
+    mileage: number;
+    totalTime: Time;
+}
 
 const constructDays = (month: number, year: number): Day[][] => {
     const today = new Date();
@@ -35,7 +39,6 @@ const constructDays = (month: number, year: number): Day[][] => {
     // Add all of the days before the first weekday
     for (let i = 0; i < firstWeekday; i++) {
         firstWeek.push({
-            exists: true,
             date: earlyNextDateToAdd,
             isToday: false,
             month: trueFirstDay.getMonth() + 1,
@@ -47,7 +50,6 @@ const constructDays = (month: number, year: number): Day[][] => {
     // Finish out adding the first week, this time with the actual days in the month
     for (let i = firstWeekday; i < 7; i++) {
         firstWeek.push({
-            exists: true,
             date: nextDateToAdd,
             isToday: (nextDateToAdd === today.getDate()) && (month - 1 === today.getMonth()) && (year === today.getFullYear()),
             month,
@@ -62,7 +64,6 @@ const constructDays = (month: number, year: number): Day[][] => {
     let weekday = 0;
     while (nextDateToAdd < daysInMonth + 1) {
         week.push({
-            exists: true,
             date: nextDateToAdd,
             isToday: nextDateToAdd === today.getDate() && month - 1 === today.getMonth() && year === today.getFullYear(),
             month,
@@ -81,7 +82,6 @@ const constructDays = (month: number, year: number): Day[][] => {
     nextDateToAdd = 1;
     while (weekday < 7 && week.length > 0) {
         week.push({
-            exists: true,
             date: nextDateToAdd,
             isToday: false,
             month: currentDate.getMonth() + 1,
@@ -98,6 +98,78 @@ const constructDays = (month: number, year: number): Day[][] => {
     return days;
 };
 
+const getActivitiesInWeek = (days: Day[], allActivities: Activity[]): Activity[] => {
+    console.log(days);
+    return allActivities.filter(a => 
+        days.filter(d => d.date === a.date.day && d.month === a.date.month && d.year === a.date.year)
+            .length > 0
+    );
+};
+
+const getWeeklySummary = (week: Activity[]): WeeklySummary => {
+    const activities = week.length;
+    const subActivities = week.reduce((prev, cur) => {
+        let count = prev;
+        if (cur.warmup) {
+            count++;
+        }
+        if (cur.cooldown) {
+            count++;
+        }
+        if (cur.strides) {
+            count++;
+        }
+        return count;
+    }, 0);
+    const mileage = week.reduce((prev, cur) => {
+        let miles = prev + distanceAsMiles(cur.distance, cur.distanceUnits);
+        if (cur.warmup) {
+            miles += distanceAsMiles(cur.warmup.distance, cur.warmup.distanceUnits);
+        }
+        if (cur.cooldown) {
+            miles += distanceAsMiles(cur.cooldown.distance, cur.cooldown.distanceUnits);
+        }
+        if (cur.strides) {
+            miles += distanceAsMiles(cur.strides.distance, cur.strides.distanceUnits);
+        }
+        return miles;
+    }, 0);
+    const totalTime = week.reduce((prev, cur) => {
+        let sum: Time = addTimes(prev, cur.time);
+        if (cur.warmup) {
+            sum = addTimes(sum, cur.warmup.time);
+        }
+        if (cur.cooldown) {
+            sum = addTimes(sum, cur.cooldown.time);
+        }
+        if (cur.strides) {
+            sum = addTimes(sum, cur.strides.time);
+        }
+        return sum;
+    }, { hours: 0, minutes: 0, seconds: 0 });
+
+    return {
+        activities,
+        subActivities,
+        mileage,
+        totalTime,
+    };
+};
+
+interface WeeklySummaryDisplayProps {
+    summary: WeeklySummary;
+}
+
+const WeeklySummaryDisplay = ({ summary }: WeeklySummaryDisplayProps) => {
+    return (
+        <>
+            <div>Activities: {summary.activities} (+{summary.subActivities})</div>
+            <div>Distance: {roundTo(summary.mileage, 2)} mi.</div>
+            <div>Time: {timeToString(summary.totalTime)}</div>
+        </>
+    );
+};
+
 interface CalendarProps {
     month: number;
     year: number;
@@ -112,15 +184,12 @@ const Calendar = ({ month, year, activities }: CalendarProps) => {
             <table className="calendar">
                 <tr>
                     {weekdays.map(d => <th key={d}>{d}</th>)}
+                    <th>Summary</th>
                 </tr>
                 {weeksToDisplay.map((week, ix) => {
                     return (
                         <tr key={ix} className="week">
                             {week.map((day, ix) => {
-                                if (!day.exists) {
-                                    return <td />;
-                                }
-
                                 const todayActivities = activities
                                     .filter(a => 
                                         a.date.day === day.date &&
@@ -136,6 +205,7 @@ const Calendar = ({ month, year, activities }: CalendarProps) => {
                                             day: true,
                                             today: day.isToday,
                                             'not-this-month': day.month !== month,
+                                            'c-pointer': true,
                                         })}
                                     >
                                         <Link
@@ -152,6 +222,11 @@ const Calendar = ({ month, year, activities }: CalendarProps) => {
                                     </td>
                                 );
                             })}
+                            <td className="day weekly-summary">
+                                <WeeklySummaryDisplay
+                                    summary={getWeeklySummary(getActivitiesInWeek(week, activities))}
+                                />
+                            </td>
                         </tr>
                     );
                 })}
